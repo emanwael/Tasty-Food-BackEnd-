767fa3189f2472aca908b4bb2f59d160abed6cc0
Israa <48530021+israaBR@users.noreply.github.com>
2023-02-02 00:45:07 +0200

    analysis creates new collection fixed

--- models/restaurant_analysis.js
+++ models/restaurant_analysis.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
 
-const analysisSchema = new mongoose.Schema({
-    restaurant_id:Number,
-    total_orders:Number,
-    meals_orders: [
-          {
-            meal_id: Number,
-            number_of_orders: Number
-          },
-        ]     
-}) 
+const analysisSchema = mongoose.Schema({
+  restaurant_id: { type: mongoose.Types.ObjectId, ref: "restaurants" },
+  total_orders: { type: Number },
+  meals_orders: {
+    type: [
+      {
+        meal_id: { type: mongoose.Types.ObjectId, ref: "meals" },
+        number_of_orders: { type: Number },
+      },
+    ],
+  },
+});
 
-const analysisModel = mongoose.model('analysis',analysisSchema);
+const analysisModel = mongoose.model("restaurant_analysis ", analysisSchema);
 module.exports = analysisModel;
--- routes/restaurant_analysis.js
+++ routes/restaurant_analysis.js
@@ -9,18 +9,18 @@ router.use(express.json());
 router.get("/", async (req, res) => {
   try {
     const getAllAnalysis = await analysisModel.find({});
-    res.json(getAllAnalysis);
+    return res.json(getAllAnalysis);
   } catch (error) {
-    res.json({ Err: "Resturant Analysis DataBase ERRORR" });
+    return res.json({ Err: "Resturant Analysis DataBase ERRORR" });
   }
 });
 
 router.get("/:id", async (req, res) => {
   try {
     const analysis = await analysisModel.findById(req.params.id);
-    res.json(analysis);
+    return res.json(analysis);
   } catch (error) {
-    res.json({ Err: "Resturant Analysis DataBase ERRORR" });
+    return res.json({ Err: "Resturant Analysis DataBase ERRORR" });
   }
 });
 
@@ -29,32 +29,33 @@ router.post("/", async (req, res) => {
 
   try {
     const savedAnalysis = await meal.save();
-    res.json(savedAnalysis);
+    return res.json(savedAnalysis);
   } catch (error) {
-    res.json({ Err: "Resturant Analysis DataBase ERRORR" });
+    return res.json({ Err: "Resturant Analysis DataBase ERRORR" });
   }
 });
 
-router.put("/:id", async(req,res)=>{
-    const id = req.params.id;
-    const body = req.body;
-    try {
-        const updatedAnalysis = await analysisModel.findByIdAndUpdate(id,body,{new:true})
-        res.json(updatedAnalysis);
-    } catch (error) {
-        res.json({ Error: "Resturant Analysis DataBase ERRORR"});
-    }
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  try {
+    const updatedAnalysis = await analysisModel.findByIdAndUpdate(id, body, {
+      new: true,
+    });
+    return res.json(updatedAnalysis);
+  } catch (error) {
+    return res.json({ Error: "Resturant Analysis DataBase ERRORR" });
+  }
+});
 
- });
-
- router.delete("/:id", async(req,res)=>{
-    const id = req.params.id;
-    try {
-        const deletedAnalysis= await analysisModel.findByIdAndDelete(id)
-        res.json(deletedAnalysis);
-    } catch (error) {
-        res.json({ Error: "Resturant Analysis DataBase ERRORR"});
-    }
- });
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const deletedAnalysis = await analysisModel.findByIdAndDelete(id);
+    return res.json(deletedAnalysis);
+  } catch (error) {
+    return res.json({ Error: "Resturant Analysis DataBase ERRORR" });
+  }
+});
 
 module.exports = router;
